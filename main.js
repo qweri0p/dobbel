95e02de6f944dac7bda34ff156ed6df247367ec1
qweri0p <71005650+qweri0p@users.noreply.github.com>
2022-04-13 09:23:49 +0200

    big update

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import './style.css'
+import rs from './rules.json'
 dorandomstuff()
 function dorandomstuff() {
-  const rng = Math.round(Math.random()*5)+1
-  const rng2 = Math.ceil(Math.random()*3) / 10
-  document.querySelector('span').innerHTML = rng+rng2
+  const rng = Math.floor(Math.random()*6)
+  const rng2 = Math.floor(Math.random()*3)
+  console.log(`${rng}, ${rng2}`)
+  document.querySelector('span').innerHTML = `Je hebt ${rng +1 + (1+rng2)/10} gedobbeld`
+  document.querySelector('#how').innerHTML = rs.how[rng][rng2]
+  document.querySelector('#what').innerHTML = rs.what[rng][rng2]
 }
-document.querySelector('button').addEventListener('click', dorandomstuff)
+document.querySelector('#wat').addEventListener('click', dorandomstuff)
