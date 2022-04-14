087f18c9800ca55bf85d5b578a509ef9b09219e4
qweri0p <71005650+qweri0p@users.noreply.github.com>
2022-04-14 15:05:41 +0200

    fixed an audit issue and now use jquery

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import './style.css'
-import rs from './rules.json'
+import rules from './rules.json'
+import $ from 'jquery'
 function dorandomstuff() {
   const rng = Math.floor(Math.random()*6)
   const rng2 = Math.floor(Math.random()*3)
   console.log(`${rng}, ${rng2}`)
-  document.querySelector('span').innerHTML = `Je hebt ${rng +1 + (1+rng2)/10} gedobbeld`
-  document.querySelector('#how').innerHTML = rs.how[rng][rng2]
-  document.querySelector('#what').innerHTML = rs.what[rng][rng2]
+  $('span').html(`Je hebt ${rng +1 + (1+rng2)/10} gedobbeld`)
+  $('#how').html(rules.how[rng][rng2])
+  $('#what').html(rules.what[rng][rng2])
 }
-document.querySelector('button').addEventListener('click', dorandomstuff)
+$('button').bind('click', dorandomstuff)
